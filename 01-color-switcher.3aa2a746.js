0533e02a584a54d7d7c47c096a07019a54c9def7
AlenaUshakova <110482834+AlenaUshakova@users.noreply.github.com>
2022-11-01 23:28:43 +0000

    Deploying to gh-pages from @ AlenaUshakova/goit-js-hw-09@fbae08d113066869c7f1530be315840b9bf3566a 🚀

---
+++ 01-color-switcher.3aa2a746.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");t.addEventListener("click",(function(){n=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,clearInterval(n)}));let n=null;
+//# sourceMappingURL=01-color-switcher.3aa2a746.js.map
